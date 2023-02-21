d3a43862b8bf6fd50601ee4ef5ae16c59760ff32
Faril Botz <125844469+FarilBotz@users.noreply.github.com>
2023-02-21 08:28:28 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,53 @@
+/**
+   * Create By Faril Botz. 
+   * Contact Me on wa.me/6287885738763
+   * Follow https://FarilBotz/FarilBotz-
+*/
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+// Website Api
+global.APIs = {
+	zenz: 'https://zenzapis.xyz',
+}
+
+// Apikey Website Api
+global.APIKeys = {
+	'https://zenzapis.xyz': 'Your Apikey',
+}
+
+// Other
+global.owner = ['13212225683']
+global.premium = ['13212225683']
+global.namaowner = ['Faril']
+global.namabot = ['FarilBotz']
+global.packname = 'Created By FarilBotz'
+global.author = 'FarilBotz'
+global.sessionName = 'FarilBotz'
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.sp = '⭔'
+global.mess = {
+    success: '✓ Success',
+    admin: 'Fitur Khusus Admin Group!',
+    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
+    owner: 'Fitur Khusus Owner Bot',
+    group: 'Fitur Digunakan Hanya Untuk Group!',
+    private: 'Fitur Digunakan Hanya Untuk Private Chat!',
+    bot: 'Fitur Khusus Pengguna Nomor Bot',
+    wait: 'Loading...',
+    endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
+}
+global.limitawal = {
+    premium: "Infinity",
+    free: 100
+}
+global.thumb = fs.readFileSync('./lib/hisoka.jpg')
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
